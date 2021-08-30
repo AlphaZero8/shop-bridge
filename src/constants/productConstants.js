10607daf4ef51a64a433e0245bf670b16aab8e1c
Viraj Bafana <viraj.bafana@team.telstra.com>
2021-08-30 20:00:15 +0530

    Shop Bridge: Add reusable components and constants 1. Create new components `<ConfirmationModal />` and `<Toast />` 2. Add constants to `appConstants.js` and `productConstants.js` NOTE: 1. This commit contains missing tests

---
+++ src/constants/productConstants.js
@@ -0,0 +1,43 @@
+// table
+export const PRODUCT_TABLE_TITLE = 'Products';
+export const PRODUCT_TABLE_HEADER_DATA = [
+    {
+        id: 'sr-no',
+        label: 'Sr. No.',
+        width: '7%',
+    },
+    {
+        id: 'name',
+        label: 'Name',
+        width: '19%'
+    },
+    {
+        id: 'description',
+        label: 'Description',
+        width: '34%'
+    },
+    {
+        id: 'manufacturer',
+        label: 'Manufacturer',
+        width: '19%'
+    },
+    {
+        id: 'price',
+        label: 'Price',
+        width: '15%',
+    },
+    {
+        id: 'actions',
+        label: 'Actions',
+        colSpan: 2,
+        width: '8%'
+    },
+];
+
+// actions
+export const TEXT_ADD_PRODUCT = 'Add Product';
+export const TEXT_VIEW_PRODUCTS = 'View Products';
+export const TEXT_ADD = 'Add';
+export const TEXT_CANCEL = 'Cancel';
+export const TEXT_SUBMIT = 'Submit';
+export const TEXT_UPDATE = 'Update';
