b1c78af6f65a8af1f73cd745b59e9b0a7525b11a
AlphaZero8 <viraj.bafana.29@gmail.com>
2023-11-20 10:50:25 +0530

    Shop bridge: Add test setup 1. Mock `axios` 2. Customize rtl's render method to provide store while testing

--- test/testUtils.js
+++ test/testUtils.js
@@ -1,1 +1,28 @@
-export const findByTestAttr = (wrapper, attrValue) => wrapper.find(`[data-test="${attrValue}"]`);
+import { render as rtlRender } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import makeStore from '../src/store/index';
+
+function render(
+    ui,
+    {
+        preloadedState,
+        store = makeStore(preloadedState),
+        ...renderOptions
+    } = {}
+) {
+    console.log(preloadedState);
+    console.log(store.getState());
+    function Wrapper({ children }) {
+        return (
+            <Provider store={store}>
+                <Router>{children}</Router>
+            </Provider>
+        );
+    }
+    return rtlRender(ui, { wrapper: Wrapper, ...renderOptions });
+}
+
+export * from '@testing-library/react';
+export { render };
